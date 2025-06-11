70d0862d4ab76be967bd604e8488903063ec5524
Pushkar Kumar Saini <pushkarkumarsaini2006@gmail.com>
2025-06-11 13:40:15 +0530

    eleventh commit

--- Task-Manager/backend/routes/authRoutes.js
+++ Task-Manager/backend/routes/authRoutes.js
@@ -15,9 +15,7 @@ router.post("/upload-image", upload.single("image"), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded" });
   }
-  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
-    req.file.filename
-  }`;
+  const imageUrl = `https://task-manager-g01e.onrender.com/uploads/${req.file.filename}`;
   res.status(200).json({ imageUrl });
 });
 
